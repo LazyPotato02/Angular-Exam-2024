f2466f286e2f2c53b91911dd0a6822da4ddcc5a3
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-11-04 22:53:43 +0200

    Added todoData model

---
+++ backend/models/todoData.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+
+const todoSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    done: { type: Boolean, default: false },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+})
+
+const todoData = mongoose.model('todoData',todoSchema)
+module.exports = todoData
