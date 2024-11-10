d4cfed8edc97f0b30f84ea787af081a153e130c9
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-11-10 18:19:26 +0200

    Updated main files for propper navigation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {RegisterComponent} from './features/auth/components/register/register.component';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  { path: '**', redirectTo: '' },
+];
+
+
