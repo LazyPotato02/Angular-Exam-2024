cebc758d26acd5baa73cf23187107e593de932cc
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-11-14 14:51:54 +0200

    Added logout functionality

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {Router} from '@angular/router';
+
+
+// TODO take interfaces to another files
 
 interface RegisterData {
   firstName: string;
@@ -19,7 +23,7 @@ interface LoginData{
 export class AuthService {
   private apiUrl = 'http://localhost:8000';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private router:Router) {}
   register(credentials: RegisterData): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/users/register`, credentials, {withCredentials: true});
   }
@@ -27,4 +31,13 @@ export class AuthService {
     return this.http.post<any>(`${this.apiUrl}/users/login`, credentials,{withCredentials: true});
   }
 
+  logout() {
+    return this.http.post(`${this.apiUrl}/users/logout`, {}, { withCredentials: true }).subscribe({
+      next: () => {
+        console.log('Logged out successfully');
+        this.router.navigate(['/login']);
+      },
+      error: (error) => console.error('Logout failed:', error),
+    });
+  }
 }
