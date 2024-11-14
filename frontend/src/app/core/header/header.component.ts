3154863c8155e7f2b52fed4a67149030c5a71e8a
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-11-14 15:18:36 +0200

    Updated header component, implemented front-end session verify and condition rendering for header

--- frontend/src/app/core/header/header.component.ts
+++ frontend/src/app/core/header/header.component.ts
@@ -1,13 +1,24 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {AuthService} from '../services/auth.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe(loggedIn => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
 
 }
--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,34 +1,52 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {BehaviorSubject, catchError, map, Observable, of, tap} from 'rxjs';
 import {Router} from '@angular/router';
+import {LoginData, RegisterData} from './auth.types';
 
 
 // TODO take interfaces to another files
 
-interface RegisterData {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-}
-interface LoginData{
-  email: string;
-  password: string;
-}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = 'http://localhost:8000';
+  private loggedInSubject = new BehaviorSubject<boolean>(false);
 
-  constructor(private http: HttpClient,private router:Router) {}
-  register(credentials: RegisterData): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/users/register`, credentials, {withCredentials: true});
+  isLoggedIn$ = this.loggedInSubject.asObservable();
+  constructor(private http: HttpClient,private router:Router) {
+
+    this.checkSession().subscribe();
   }
+
+  register(credentials: RegisterData): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/users/register`, credentials, { withCredentials: true }).pipe(
+      tap(() => {
+        this.loggedInSubject.next(true);
+        this.router.navigate(['/']);
+      }),
+      catchError((error) => {
+        console.error('Registration failed:', error);
+        this.loggedInSubject.next(false);
+        return of(error);
+      })
+    );
+  }
+
   login(credentials: LoginData): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/users/login`, credentials,{withCredentials: true});
+    return this.http.post<any>(`${this.apiUrl}/users/login`, credentials, { withCredentials: true }).pipe(
+      tap(() => {
+        this.loggedInSubject.next(true);
+        this.router.navigate(['/']); 
+      }),
+      catchError((error) => {
+        console.error('Login failed:', error);
+        this.loggedInSubject.next(false);
+        return of(error);
+      })
+    );
   }
 
   logout() {
@@ -36,8 +54,21 @@ export class AuthService {
       next: () => {
         console.log('Logged out successfully');
         this.router.navigate(['/login']);
+        this.loggedInSubject.next(false)
       },
       error: (error) => console.error('Logout failed:', error),
     });
   }
+
+  checkSession(): Observable<boolean> {
+    return this.http.get<{ loggedIn: boolean }>(`${this.apiUrl}/users/verify`, { withCredentials: true }).pipe(
+      map(response => response.loggedIn),
+      tap(loggedIn => this.loggedInSubject.next(loggedIn)),
+      catchError(() => {
+        this.loggedInSubject.next(false);
+        return of(false);
+      })
+    );
+  }
+
 }
